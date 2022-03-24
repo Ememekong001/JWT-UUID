bd8a4400fa366f6c78838ac02636b5e09e5bea1f
Ememekong <airmyekong@gmail.com>
2022-03-24 16:33:24 +0100

    Modified the migrations, created funtions on the roles model and created the middleware for Roles

--- database/migrations/2022_03_22_173144_add_roles_to_users_table.php
+++ database/migrations/2022_03_22_173144_add_roles_to_users_table.php
@@ -14,13 +14,12 @@ class AddRolesToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-             $table->unsignedBigInteger('role_id');
+            $table->integer('role_id')->unsigned();
 
              $table->foreign('role_id')
                 ->references('id')
                 ->on('roles')
-                ->onDelete('cascade');
-
+                ->onDelete('CASCADE');
 
         });
     }
--- database/seeders/ExecoSeeder.php
+++ database/seeders/ExecoSeeder.php
@@ -20,71 +20,78 @@ class ExecoSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'Administrator',
             'email' => 'admin@gmail.com',
+            'user_id' => Str::Uuid()->toString(),
             'password' => Hash::make('adminpass'),
             'age' => '52',
             'address'=> '10 Udo Eduok',
             'phone' => '09094502345',
-            'role_id' => '1'
+            'role_id' => 1
         ]);
 
-    //     DB::table('users')->insert([
-    //         'name' => 'Doctor',
-    //         'email' => 'doctor@gmail.com',
-    //         'password' => Hash::make('docotorpass'),
-    //         'age' => '25',
-    //         'address'=> '10 Udo Eduok',
-    //         'phone' => '08089670445',
-    //         'role_id' => '2'
-    //     ]);
+        DB::table('users')->insert([
+            'user_id' => Str::Uuid()->toString(),
+            'name' => 'Doctor',
+            'email' => 'doctor@gmail.com',
+            'password' => Hash::make('docotorpass'),
+            'age' => '25',
+            'address'=> '10 Udo Eduok',
+            'phone' => '08089670445',
+            'role_id' => 2
+        ]);
 
-    //     DB::table('users')->insert([
-    //         'name' => 'Nurse',
-    //         'email' => 'nurse@gmail.com',
-    //         'password' => Hash::make('nursepass'),
-    //         'age' => '20',
-    //         'address'=> '10 Udo Eduok',
-    //         'phone' => '09094502345',
-    //         'role_id' => '3'
-    //     ]);
+        DB::table('users')->insert([
+            'user_id' => Str::Uuid()->toString(),
+            'name' => 'Nurse',
+            'email' => 'nurse@gmail.com',
+            'password' => Hash::make('nursepass'),
+            'age' => '20',
+            'address'=> '10 Udo Eduok',
+            'phone' => '09094502345',
+            'role_id' => '3'
+        ]);
 
-    //     DB::table('users')->insert([
-    //         'name' => 'Pharmacist',
-    //         'email' => 'pharm@gmail.com',
-    //         'password' => Hash::make('pharmpass'),
-    //         'age' => '27',
-    //         'address'=> '10 Udo Eduok',
-    //         'phone' => '08089670446',
-    //         'role_id' => '4'
-    //     ]);
+        DB::table('users')->insert([
+            'user_id' => Str::Uuid()->toString(),
+            'name' => 'Pharmacist',
+            'email' => 'pharm@gmail.com',
+            'password' => Hash::make('pharmpass'),
+            'age' => '27',
+            'address'=> '10 Udo Eduok',
+            'phone' => '08089670446',
+            'role_id' => '4'
+        ]);
 
-    //     DB::table('users')->insert([
-    //         'name' => 'Receptionist',
-    //         'email' => 'reception@gmail.com',
-    //         'password' => Hash::make('adminpass'),
-    //         'age' => '25',
-    //         'address'=> '10 Udo Eduok',
-    //         'phone' => '09094502345',
-    //         'role_id' => '5'
-    //     ]);
+        DB::table('users')->insert([
+            'user_id' => Str::Uuid()->toString(),
+            'name' => 'Receptionist',
+            'email' => 'reception@gmail.com',
+            'password' => Hash::make('adminpass'),
+            'age' => '25',
+            'address'=> '10 Udo Eduok',
+            'phone' => '09094502345',
+            'role_id' => '5'
+        ]);
 
-    //     DB::table('users')->insert([
-    //         'name' => 'StudentDoctor',
-    //         'email' => 'student@gmail.com',
-    //         'password' => Hash::make('studentpass'),
-    //         'age' => '25',
-    //         'address'=> '10 Udo Eduok',
-    //         'phone' => '09094502345',
-    //         'role_id' => '6'
-    //     ]);
+        DB::table('users')->insert([
+            'user_id' => Str::Uuid()->toString(),
+            'name' => 'StudentDoctor',
+            'email' => 'student@gmail.com',
+            'password' => Hash::make('studentpass'),
+            'age' => '25',
+            'address'=> '10 Udo Eduok',
+            'phone' => '09094502345',
+            'role_id' => '6'
+        ]);
 
-    //     DB::table('users')->insert([
-    //         'name' => 'User',
-    //         'email' => 'user@gmail.com',
-    //         'password' => Hash::make('userpass'),
-    //         'age' => '25',
-    //         'address'=> '10 Udo Eduok',
-    //         'phone' => '09094502345',
-    //         'role_id' => '7'
-    //     ]);
+        DB::table('users')->insert([
+            'user_id' => Str::Uuid()->toString(),
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('userpass'),
+            'age' => '25',
+            'address'=> '10 Udo Eduok',
+            'phone' => '09094502345',
+            'role_id' => '7'
+        ]);
     }
 }
